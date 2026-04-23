a76155a19a77633c8bf6163b0ee99f480a1a16b5
Arnel <arnelvergara1234@gmail.com>
2026-04-23 10:26:17 +0800

    header.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,18 +38,13 @@ const Header = () => {
           </Link>
         </nav>
         
-        <div className="user-section">
-          <div className="user-avatar">
-            <span>{user?.name?.charAt(0).toUpperCase()}</span>
-          </div>
-          <button onClick={handleLogout} className="logout-btn">
-            <svg className="logout-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-              <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4M16 17l4-4-4-4M16 7V4a2 2 0 0 0-2-2h-4" />
-              <path d="M12 12h8" />
-            </svg>
-            <span>Sign Out</span>
-          </button>
-        </div>
+        <button onClick={handleLogout} className="logout-btn">
+          <svg className="logout-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+            <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4M16 17l4-4-4-4M16 7V4a2 2 0 0 0-2-2h-4" />
+            <path d="M12 12h8" />
+          </svg>
+          <span>Sign Out</span>
+        </button>
       </div>
     </header>
   );
